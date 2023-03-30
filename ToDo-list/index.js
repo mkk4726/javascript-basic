50ccd590804e9d8cd1624c54930d5efd504de10e
mkk4726 <mkk4726@naver.com>
2023-03-30 17:19:18 +0900

    Add button function using addEventListener

--- ToDo-list/index.js
+++ ToDo-list/index.js
@@ -1,15 +1,24 @@
-const todoInput = document.getElementById("todo-list")
+const todoInput = document.getElementById("todo-input")
+
+const createTodo = () => {
+  const todoList = document.querySelector('#todo-list')
+  const newLi = document.createElement('li');
+  const newSpan = document.createElement('span');
+  const newBtn = document.createElement('button')
+
+  newBtn.addEventListener('click', () => {
+    newLi.classList.toggle('complete');
+  })
+
+  newSpan.textContent = todoInput.value;
+  newLi.appendChild(newBtn)
+  newLi.appendChild(newSpan);
+  todoList.appendChild(newLi); 
+  todoInput.value = ''
+}
 
 const keyCodeCheck = () => {
-  const inputValue = document.querySelector('#todo-input').value;
-  if (window.event.keyCode === 13 && inputValue !=='') {
-    const newLi = document.createElement('li');
-    const newSpan = document.createElement('span');
-
-    newSpan.textContent = inputValue;
-    newLi.appendChild(newSpan);
-    todoInput.appendChild(newLi);    
-
-
+  if (window.event.keyCode === 13 && todoInput.value) {
+       createTodo();
   }
 }
