d77971950f2719da7acadca1dbc8a536e2ccd7c8
mkk4726 <mkk4726@naver.com>
2023-03-30 18:21:52 +0900

    Add delete function using .children, remove method.

--- ToDo-list/index.js
+++ ToDo-list/index.js
@@ -1,13 +1,17 @@
 const todoInput = document.getElementById("todo-input")
+const todoList = document.querySelector('#todo-list')
 
-const createTodo = () => {
-  const todoList = document.querySelector('#todo-list')
+const createTodo = () => {  
   const newLi = document.createElement('li');
   const newSpan = document.createElement('span');
   const newBtn = document.createElement('button')
 
   newBtn.addEventListener('click', () => {
     newLi.classList.toggle('complete');
+  })
+
+  newLi.addEventListener('dblclick', () => {
+    newLi.remove();
   })
 
   newSpan.textContent = todoInput.value;
@@ -20,5 +24,24 @@ const createTodo = () => {
 const keyCodeCheck = () => {
   if (window.event.keyCode === 13 && todoInput.value) {
        createTodo();
+  }
+}
+
+const deleteAll = () => {
+  const liList = document.querySelectorAll('li');
+  for (let li of liList) {
+    li.remove();
+  }
+}
+
+
+const saveItems = () => {
+  const saveItems = [];
+  for (let tag of todoList.children) {
+    const todoObj = {
+      contents: tag.querySelector('span').textContent,
+      complete: tag.classList.contains('complete')
+    }
+    saveItems.push(todoObj);
   }
 }
