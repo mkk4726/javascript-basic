992abbe4459f36cb1e65a8e1384006191515c149
mkk4726 <mkk4726@naver.com>
2023-03-31 09:18:59 +0900

    Update get weather function using weatherAPI

--- ToDo-list/index.js
+++ ToDo-list/index.js
@@ -71,35 +71,41 @@ if (savedTodoList) {
     createTodo(list);
   }
 }
+const apiKey = 'b75c314757d971db1a7dd9cb39fb791f';
+const apiKey_default = 'a50ebb9af615c1ed1152e278f7b7b8bf';
+
+const weatherSearch = (pos, apiKey) => {
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${pos.lat}&lon=${pos.lon}&appid=${apiKey}`;
+  fetch(url)
+    .then((res) => {
+      // console.log(res.json());
+      return res.json();
+    })
+    .then((json) => {
+      console.log(json.name, json.weather[0].description);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+}
 
 const success = (pos) => {
   const crd = pos.coords;
+  const position = {
+    lat: crd.latitude,
+    lon : crd.longitude
+  }
 
-  console.log("Your current position is:");
-  console.log(`Latitude : ${crd.latitude}`);
-  console.log(`Longitude: ${crd.longitude}`);
-  console.log(`More or less ${crd.accuracy} meters.`);
+  weatherSearch(position, apiKey_default);
 };
 
 const error = (err) => {
   console.warn(`ERROR(${err.code}): ${err.message}`);
 }
 
-const options = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 0,
-};
 
 const askForLocation = () => {
-  navigator.geolocation.getCurrentPosition(success, error, options)
+  navigator.geolocation.getCurrentPosition(success, error)
 }
 
 askForLocation();
-
-const apiKey = 'a50ebb9af615c1ed1152e278f7b7b8bf';
-
-const weatherSearch = (lat, lon, part, apiKey) => {
-  const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${apiKey}`;
-  
-}
